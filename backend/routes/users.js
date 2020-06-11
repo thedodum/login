3472064db39083d5d108d71036ed012a1ed192f9
jeonyk <jeonyk@neoforth.com>
2020-06-11 11:03:47 +0900

    login, join, list

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,114 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var bcrypt = require('bcrypt');
+
+// Connection 객체 생성
+var connection = mysql.createConnection({
+  host: '61.251.164.24',
+  port: 3306,
+  user: 'neo',
+  password: 'neo!!!',
+  database: 'TEST_DB'
+});
+
+// Connect
+connection.connect(function (err) {
+  if (err) {
+    console.error('mysql connection error');
+    console.error(err);
+    throw err;
+  }
+});
+
+// insert
+/*app.post('/regist', function (req, res) {
+  var user = {
+    'userid': req.body.userid,
+    'name': req.body.name,
+    'address': req.body.address
+  };
+  var query = connection.query('insert into users set ?', user, function (err, result) {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    res.status(200).send('success');
+  });
+});*/
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});*/
+
+router.get('/', function (req, res) {
+  connection.query('SELECT * FROM member', function (err, rows) {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
+
+router.post('/signUp', function (req, res) {
+  // console.log(req);
+  const user = {
+    'userid': req.body.user.userid,
+    'name': req.body.user.name,
+    'password': req.body.user.password
+  };
+  connection.query('SELECT userid FROM member WHERE userid = "' + user.userid + '"', function (err, row) {
+    if (row[0] == undefined){ //  동일한 아이디가 없을경우,
+      const salt = bcrypt.genSaltSync();
+      const encryptedPassword = bcrypt.hashSync(user.password, salt);
+      connection.query('INSERT INTO member (userid,name,password) VALUES ("' + user.userid + '","' + user.name + '","' + encryptedPassword + '")', user, function (err, row2) {
+        if (err) throw err;
+      });
+      res.json({
+        success: true,
+        message: 'Sing Up Success!'
+      })
+    }
+    else {
+      res.json({
+        success: false,
+        message: 'Sign Up Failed Please use anoter ID'
+      })
+    }
+  });
+});
+
+router.post('/login', function (req, res) {
+  const user = {
+    'userid': req.body.user.userid,
+    'password': req.body.user.password
+  };
+  connection.query('SELECT userid, password FROM member WHERE userid = "' + user.userid + '"', function (err, row) {
+
+    if (err) {
+      res.json({ // 매칭되는 아이디 없을 경우
+        success: false,
+        message: 'Login failed please check your id!'
+      })
+    }
+
+    if (row[0] !== undefined && row[0].userid === user.userid) {
+      bcrypt.compare(user.password, row[0].password, function (err, res2) {
+        if (res2) {
+          res.json({ // 로그인 성공
+            success: true,
+            message: 'Login successful!'
+          })
+        }
+        else {
+          res.json({ // 매칭되는 아이디는 있으나, 비밀번호가 틀린 경우
+            success: false,
+            message: 'Login failed please check your password!'
+          })
+        }
+      })
+    }
+  })
 });
 
 module.exports = router;
